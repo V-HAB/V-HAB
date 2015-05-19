c0f2ded682d946398c0009b61fa109a78e95da0e
Claas Olthoff <c.olthoff@tum.de>
2015-05-19 12:39:02 +0200

    Update hydraulic/linear solver to work with active, but non-pressure-rise-producing f2f processors
    - When creating the hydraulic solver type, we had the fan in mind as an active component. It is both in need of being updated regularly and produces a pressure rise. Therefore, the update method handle had fDeltaPressure as a mandatory return variable.
    - There are, however, other active components, that do not produce a pressure rise, but still require regular updates. An example for this is the valve in the PLSS regulator, which varies its hydraulic diameter to produce the desired pressure drop.
    - This commit adds an if-condition that provides the fDeltaPressure return variable for active, pressure-rise-producing processors and just calls the update method for the other components.

--- core/+solver/+matter/+base/+type/hydraulic.m
+++ core/+solver/+matter/+base/+type/hydraulic.m
@@ -18,15 +18,27 @@ classdef hydraulic < handle
             this.fHydrLength  = fHydrLength;
             % Active component? Needs fct handle to calculate delta p
             if nargin >= 3 && bActive
-                this.bActive             = bActive;
+                this.bActive                = bActive;
                 this.calculateDeltaPressure = calcMethod;
             end
         end
         
         
         function fDeltaPress = updateDeltaPressure(this)
-            this.fDeltaPressure = this.calculateDeltaPressure();
-            fDeltaPress = this.fDeltaPressure;
+            % We only need the delta pressure if the active component
+            % causes a pressure rise, like a fan. If the active component
+            % is for instance a valve, it will just change its hydraulic
+            % diameter or length when its update method is called, but it
+            % will not calculate a delta pressure. 
+            if nargout > 0
+                this.fDeltaPressure = this.calculateDeltaPressure();
+                fDeltaPress = this.fDeltaPressure;
+            else
+                % The component is active, but doesn't produce a pressure
+                % rise, so we just call the update method and return
+                % nothing. 
+                this.calculateDeltaPressure();
+            end
         end
     end
 end
