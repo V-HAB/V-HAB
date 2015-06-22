c1cdc859bc75494d5e344897f4d22860b695903a
Jonas Schnaitmann <j.schnaitmann@lrt.mw.tum.de>
2015-06-22 12:30:12 +0200

    First version of the residual solver
    Can be used once for a phase, solver should be added as the last solver.
    Sums up flowrates of branches (including p2ps) and sets this value as flow rate.
    
    Note: only checks/works for the "left" phase, has to be added accordingly in .createBranch().
    Will not be 100% exact due to rounding errors (at least that is the excuse).

---
+++ core/+solver/+matter/+residual/branch.m
@@ -0,0 +1,46 @@
+classdef branch < solver.matter.manual.branch
+    % Calculates sum of flow rates of other branches and p2p procs and set
+    % remaining flowrate for own branch.
+    %
+    %TODO
+    %   * at the moment, using the 'left' phase to calculate flow rate.
+    %     Make user-selectable?
+    %   * check if in phase is bSynced, check if added as last solver?
+    
+    properties (SetAccess = protected, GetAccess = public)
+        
+    end
+    
+    
+    methods
+        function this = branch(oBranch)
+            this@solver.matter.manual.branch(oBranch);
+        end
+    end
+    
+    methods (Access = protected)
+        function update(this)
+            % CALC GET THE FLOW RATE
+            fResidualFlowRate  = 0;
+            oPhase             = this.oBranch.coExmes{1}.oPhase;
+            
+            % Branches and p2p flows - they're also branches!
+            for iE = 1:oPhase.iProcsEXME
+                oExme   = oPhase.coProcsEXME{iE};
+                oBranch = oExme.aoFlows(1).oBranch;
+                
+                if oBranch == this.oBranch
+                    continue;
+                end
+                
+                fResidualFlowRate = fResidualFlowRate + oExme.aiSign(1) * oExme.aoFlows(1).fFlowRate;
+            end
+            
+            this.fRequestedFlowRate = fResidualFlowRate;
+            
+            %fprintf('[%fs] Branch %s Residual Solver - set Flow Rate %f\n', this.oBranch.oContainer.oData.oTimer.fTime, this.oBranch.sName, this.fRequestedFlowRate);
+            
+            update@solver.matter.manual.branch(this);
+        end
+    end
+end
