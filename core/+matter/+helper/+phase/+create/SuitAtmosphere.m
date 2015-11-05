25a8548846280d88782ba39bfe8dd3e246a6c537
Claas Olthoff <c.olthoff@tum.de>
2015-11-05 18:12:41 +0100

    Updated SuitAtmosphere helper to include relative humidity

--- core/+matter/+helper/+phase/+create/SuitAtmosphere.m
+++ core/+matter/+helper/+phase/+create/SuitAtmosphere.m
@@ -1,4 +1,4 @@
-function [ cParams, sDefaultPhase ] = SuitAtmosphere(~, fVolume, fTemperature, rRH, fPressure)
+function [ cParams, sDefaultPhase ] = SuitAtmosphere(oStore, fVolume, fTemperature, rRH, fPressure)
 %SUITATMOSPHERE helper to create a matter phase with a standard space suit
 %   atmosphere using 100% oxygen.
 %   If just volume given, created as a 100% oxygen atmosphere at 29647 Pa, 
@@ -11,35 +11,65 @@ function [ cParams, sDefaultPhase ] = SuitAtmosphere(~, fVolume, fTemperature, r
 %   fPressure       - Pressure in Pa - default 29647 Pa
 
 % Values from @matter.table
-fRm         = 8.314472;
-fMolMassH2O = 18;
-fMolMassO2  = 32;
-fMolMassCO2 = 44;
-fMolMassN2  = 28;
+% ideal gas constant [J/K]
+fRm = oStore.oMT.Const.fUniversalGas; 
+
+% Molecular mass of water in [kg/mol]
+fMolarMassH2O = oStore.oMT.ttxMatter.H2O.fMolarMass; 
+
+% Molecular mass of oxygen in [kg/mol]
+fMolarMassO2 = oStore.oMT.ttxMatter.O2.fMolarMass; 
 
 % Check input arguments, set default
-%TODO for fTemp, rRH, fPress -> key/value pairs?
-if nargin < 3, fTemperature = 293.15; end;
-if nargin < 4, rRH          = 0;      end;
-if nargin < 5, fPressure    = 28300; end;
+if nargin < 3 || isempty(fTemperature), fTemperature = matter.table.Standard.Temperature; end;
+if nargin < 4 || isempty(rRH),          rRH          = 0;      end;
+if nargin < 5 || isempty(fPressure),    fPressure    = matter.table.Standard.Pressure; end;
 
-% p V = m / M * R_m * T -> mol mass in g/mol so divide
-fMass = fPressure * fVolume * (fMolMassO2 / 1000) / fRm / fTemperature;
-
+if rRH
+    % Calculation of the saturation vapour pressure
+    % by using the MAGNUS Formula(validity: -45degC <= T <= 60degC, for
+    % water); Formula is only correct for pure steam, not the mixture
+    % of air and water; enhancement factors can be used by a
+    % Poynting-Correction (pressure and temperature dependent); the values of the enhancement factors are in
+    % the range of 1+- 10^-3; thus they are neglected.
+    % Formula is also only correct for water and air, not pure oxygen.
+    %Source: Important new Values of the Physical Constants of 1986, Vapour
+    % Pressure Formulations based on ITS-90, and Psychrometer Formulae. In: Z. Meteorol.
+    % 40, 5, S. 340-344, (1990)
+    
+    fSaturationVapourPressure = 6.11213 * exp(17.62 * (fTemperature-273.15) / (243.12 + (fTemperature-273.15))) * 100;
+    
+    % calculate vapour pressure [Pa]
+    fVapourPressure = rRH*fSaturationVapourPressure; 
+    
+    % calculate mass fraction of H2O in air
+    fMassFractionH2O = fMolarMassH2O/fMolarMassO2*fVapourPressure/(fPressure-fVapourPressure);
+    
+    % calculate molar fraction of H2O in air
+    fMolarFractionH2O = fMassFractionH2O/fMolarMassH2O*fMolarMassO2; 
+    
+    % p V = m / M * R_m * T -> mol mass in g/mol so divide p*V=n*R*T;
+    
+    %calculate total mass
+    fMassGes = (fPressure) * fVolume * ((fMolarFractionH2O*fMolarMassH2O+(1-fMolarFractionH2O)*fMolarMassO2)) / fRm / fTemperature; 
+    
+    % calculate dry air mass
+    fMass = fMassGes*(1-fMassFractionH2O); 
+    
+else
+    fMass = fPressure * fVolume * fMolarMassO2 / fRm / fTemperature;
+    
+    % Need to set this to zero in case of dry gas
+    fMassGes = 0;
+    fMassFractionH2O = 0;
+end
 % Matter composition
 tfMass = struct(...
-    'O2', fMass * 0.999, ...
-    'CO2', fMass * 0.001 ...
-    ...'O2', fMass ...
-);
-
-% Check relative humidity - add? For now its just zero.
-% See http://en.wikipedia.org/wiki/Vapor_pressure
-% http://de.wikipedia.org/wiki/Ambrose-Walton-Methode
-if rRH > 0
-    tfMass.H2O = 0;
-end
-
+    'O2',  0.999 * fMass, ...
+    'CO2', 0.001 * fMass ...
+    );
+% Calculate H2O mass if present 
+tfMass.H2O = fMassGes * fMassFractionH2O; 
 
 % Create cParams for a whole matter.phases.gas standard phase. If user does
 % not want to use all of them, can just use
