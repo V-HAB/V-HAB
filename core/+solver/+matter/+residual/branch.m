10212a758efd9107e86e1d0d351e4af2faeac1bc
Claas Olthoff <c.olthoff@tum.de>
2016-01-17 15:17:43 +0100

    Updated the residual solver branch to make the direction of flow user-selectable.
    Positive flow (left to right) is the default value, using the setPositiveFlowDirection() method, the user can set a boolean variable to false. This will cause the residual solver branch to use the right side exme processor and its attached phase as the source for its flow rate calculations.

--- core/+solver/+matter/+residual/branch.m
+++ core/+solver/+matter/+residual/branch.m
@@ -3,11 +3,14 @@ classdef branch < solver.matter.manual.branch
     % remaining flowrate for own branch.
     %
     %TODO
-    %   * at the moment, using the 'left' phase to calculate flow rate.
-    %     Make user-selectable?
     %   * check if in phase is bSynced, check if added as last solver?
     
     properties (SetAccess = protected, GetAccess = public)
+        % Boolean variable to set if the residual flow rate is calculated
+        % using the 'left' or the 'right' phase with respect to the matter
+        % branch object. Can be changed by the user using the 
+        % setPositiveFlowDirection() method. 
+        bPositiveFlowDirection = true;
         
     end
     
@@ -19,13 +22,26 @@ classdef branch < solver.matter.manual.branch
             
             this.iPostTickPriority = -1;
         end
+        
+        function setPositiveFlowDirection(this, bPositiveFlowDirection)
+            this.bPositiveFlowDirection = bPositiveFlowDirection;
+        end
     end
     
     methods (Access = protected)
         function update(this)
             % CALC GET THE FLOW RATE
             fResidualFlowRate  = 0;
-            oPhase             = this.oBranch.coExmes{1}.oPhase;
+            
+            if this.bPositiveFlowDirection
+                iExme = 1;
+                iDir  = 1;
+            else 
+                iExme = 2;
+                iDir  = -1;
+            end
+            
+            oPhase             = this.oBranch.coExmes{iExme}.oPhase;
             
             % Branches and p2p flows - they're also branches!
             for iE = 1:oPhase.iProcsEXME
@@ -39,11 +55,12 @@ classdef branch < solver.matter.manual.branch
                 fResidualFlowRate = fResidualFlowRate + oExme.iSign * oExme.oFlow.fFlowRate;
             end
             
-            this.fRequestedFlowRate = fResidualFlowRate;
+            this.fRequestedFlowRate = fResidualFlowRate * iDir;
             
             %fprintf('[%fs] Branch %s Residual Solver - set Flow Rate %f\n', this.oBranch.oContainer.oData.oTimer.fTime, this.oBranch.sName, this.fRequestedFlowRate);
             
             update@solver.matter.manual.branch(this);
         end
-    end
+        end
+    
 end
