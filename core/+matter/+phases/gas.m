fa709a6c3f5d1a46ed2e6caf9013f7174b7599ec
Claas Olthoff <c.olthoff@tum.de>
2016-01-19 12:29:06 +0100

    Fixed an error in the use of the Magnus formula in phase.gas. Temperatures here have to be in degrees Celsius, where set to Kelvin at some point.

--- core/+matter/+phases/gas.m
+++ core/+matter/+phases/gas.m
@@ -122,7 +122,7 @@ classdef gas < matter.phase
                 if this.afMass(this.oMT.tiN2I.H2O)
                     % calculate saturation vapour pressure [Pa];
                     % MAGNUS Formula; validity: -45???C <= T <= 60???C, for water
-                    fSaturationVapourPressure = 6.11213 * exp(17.62 * this.fTemperature / (243.12 + this.fTemperature)) * 100;
+                    fSaturationVapourPressure = 6.11213 * exp(17.62 * this.fTemperature - 273.15 / (243.12 + this.fTemperature - 273.15)) * 100;
                     % calculate relative humidity
                     this.rRelHumidity = this.afPP(this.oMT.tiN2I.H2O) / fSaturationVapourPressure;
                 else
