4c007790d7c12d4dca8a15637a7d11696697d3a1
Claas Olthoff <c.olthoff@tum.de>
2016-01-22 13:07:19 +0100

    Added a tool to calculate the volume of a solid substance when given the masses and temperature.

---
+++ core/+matter/@table/calculateSolidVolume.m
@@ -0,0 +1,27 @@
+function [ fVolume ] = calculateSolidVolume(this, tfMasses, fTemperature)
+%calculates the volume of a solid phase with the given tf masses struct
+%   since no volume can be set for solid phases this function can be used
+%   to calculate the solid volume beforehand and implement it in the store
+%   definition to achieve a fitting overal store volume with defineable
+%   gas/liquid volumes
+
+% gets the names of the substances used in this solid
+csSubstances = fieldnames(tfMasses);
+
+% creates the afMass variable
+afMass = zeros(1,this.iSubstances);
+for iSubstance = 1:length(csSubstances)
+    
+    iIndex = this.tiN2I.(csSubstances{iSubstance});
+    
+    afMass(iIndex) = tfMasses.(csSubstances{iSubstance});
+    
+end
+% calculates the density
+fDensity = this.calculateDensity('solid',afMass,fTemperature);
+
+% The volume is the mass divided with the density
+fVolume = sum(afMass)/fDensity;
+
+end
+
