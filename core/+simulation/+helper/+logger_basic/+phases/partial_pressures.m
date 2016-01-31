f6920235c9b7c1aad5ded2905ce86e2060263ef3
Claas Olthoff <c.olthoff@tum.de>
2016-01-31 22:40:53 +0100

    Added a logging helper that logs the partial pressure in a given phase for given substances.

---
+++ core/+simulation/+helper/+logger_basic/+phases/partial_pressures.m
@@ -0,0 +1,41 @@
+function tLogProps = partial_pressures(tLogProps, oPhase, csSubstances)
+%PARTIAL_PRESSURES Summary of this function goes here
+%   Detailed explanation goes here
+
+
+if ~isa(oPhase, 'matter.phases.gas')
+    error('The provided phase (%s %s) is not a gas phase. Partial pressures can only be logged in gas phases.', oPhase.oStore.sName, oPhase.sName);
+end
+
+oStore = oPhase.oStore;
+oVsys  = oStore.oContainer;
+sPath  = simulation.helper.paths.getSysPath(oVsys);
+
+
+if nargin < 3 || isempty(csSubstances)
+    csSubstances = { 'N2', 'O2', 'CO2', 'Ar' };
+end
+
+
+
+iV = 1;
+sStorePath = [ sPath '.toStores.' oPhase.oStore.sName ];
+sPhasePath = [ sStorePath '.toPhases.' oPhase.sName ];
+
+for iC = 1:length(csSubstances);
+    
+    tLogProps(iV).sObjectPath = sPhasePath;
+    tLogProps(iV).sExpression = [ 'afPP(' num2str(oVsys.oMT.tiN2I.(csSubstances{iC})) ')' ];
+    tLogProps(iV).sLabel = [ 'Phase ' csSubstances{iC} ' Partial Pressure (' oVsys.sName ' - ' oStore.sName ' - ' oPhase.sName ')' ];
+    
+    
+    iV = iV + 1;
+    
+    
+end
+
+
+
+
+end
+
