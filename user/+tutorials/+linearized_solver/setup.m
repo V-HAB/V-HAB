1c535763b41bbbc1021ada4c561b08120737096c
Jonas Schnaitmann <j.schnaitmann@lrt.mw.tum.de>
2016-04-10 11:42:14 +0200

    Multi Solver Example Tuts

--- user/+tutorials/+linearized_solver/setup.m
+++ user/+tutorials/+linearized_solver/setup.m
@@ -4,22 +4,11 @@ classdef setup < simulation.infrastructure
     end
     
     methods
-        function this = setup(ptConfigParams, tSolverParams, fSimTime, bExample2) % Constructor function
+        function this = setup(ptConfigParams, tSolverParams, fSimTime) % Constructor function
             
+            this@simulation.infrastructure('Tutorial_Linearized_Solver', ptConfigParams, tSolverParams);
             
-            % Example for _Mutiple
-            % vhab.exec('tutorials.valve_examples.setup', [], struct('rUpdateFrequency', 0.05, 'rHighestMaxChangeDecrease', 500), 3600 * 0.1, true)
-            % vhab.exec('tutorials.valve_examples.setup', containers.Map({'Example'}, {struct('piPipeLengths', containers.Map({ 6, 7, 9 }, { 10, 10, 10 }))}), struct('rUpdateFrequency', 0.05, 'rHighestMaxChangeDecrease', 500), 3600 * 0.1, true)
-            
-            this@simulation.infrastructure('Tutorial_Valve_Examples', ptConfigParams, tSolverParams);
-            
-            % Creating the 'Example' system as a child of the root system
-            % of this simulation. 
-            if (nargin >= 4) && ~isempty(bExample2) && islogical(bExample2) && (bExample2 == true)
-                tutorials.valve_examples.systems.Example_Multiple(this.oSimulationContainer, 'Example');
-            else
-                tutorials.valve_examples.systems.Example(this.oSimulationContainer, 'Example');
-            end
+            tutorials.linearized_solver.systems.Example(this.oSimulationContainer, 'Example_Sys');
             
             
             
@@ -27,7 +16,7 @@ classdef setup < simulation.infrastructure
             %% Simulation length
             
             if nargin < 3 || isempty(fSimTime)
-                fSimTime = 3600 * 3;
+                fSimTime = 3600;
             end
             
             % Stop when specific time in simulation is reached or after 
@@ -41,6 +30,9 @@ classdef setup < simulation.infrastructure
         
         function configureMonitors(this)
             
+            %this.toMonitors.oConsoleOutput.setReportingInterval(10, 1);
+            
+            
             %% Logging
             % Creating a cell setting the log items. You need to know the
             % exact structure of your model to set log items, so do this
@@ -48,12 +40,7 @@ classdef setup < simulation.infrastructure
             
             oLog = this.toMonitors.oLogger;
             
-            tiFlowProps = oLog.add('Example', 'flow_props');
-            
-            % Add single values
-            iPropLogIndex1 = oLog.addValue('Example', 'iChildren',     [],  'Label of Prop');
-            %keyboard();
-            iPropLogIndex2 = oLog.addValue('Example', 'fPipeDiameter', 'm', 'Pipe Diameter');
+            tiFlowProps = oLog.add('Example_Sys', 'flow_props');
             
             
             
@@ -76,8 +63,6 @@ classdef setup < simulation.infrastructure
             
             this.toMonitors.oPlotter.plot();
             
-            return;
-            %tools.arrangeWindows();
         end
         
     end
