9235a5b1d61c51a320f2ca985749ae93c3542354
Daniel Pütz <d.puetz@tum.de>
2018-04-22 10:55:56 +0200

    - Implemented new iterative solver which uses nested intervals to find a solution, this solver is now used in the fan loop flow tutorial - fixed fan loop flow tutorial

---
+++ core/+solver/+matter/+interval/branch.m
@@ -0,0 +1,250 @@
+classdef branch < solver.matter.base.branch
+    %BRANCH based on the nested interval principle for matter flows. It is
+    % therefore similar to the iterative solver in the final solution
+    % (pressure drops are equal to pressure difference from phases and
+    % pressure rises from components) but uses a completly different
+    % solution method
+
+    properties (SetAccess = public, GetAccess = public)
+        
+        
+        
+    end
+    
+    properties (SetAccess = protected, GetAccess = public)
+        % Actual time between flow rate calculations
+        fTimeStep = 0;
+        
+        fMaxError       = 1;    % Maximum allowed error in the solution in Pa
+        iMaxIterations  = 500;  % Maximum allowed iterations
+    end
+    methods
+        
+        %% Constructor
+        function this = branch(oBranch, fInitialFlowRate)
+            
+            if nargin < 2
+                fInitialFlowRate = [];
+            end
+            
+            this@solver.matter.base.branch(oBranch, fInitialFlowRate, 'callback');
+            
+            if this.oBranch.iFlowProcs == 0
+                this.warn('There are no f2f processors in the iterative solver branch %s.\nThis may cause problems during flow rate calculation.\nIt is recommended to insert a small pipe.');
+            end
+            
+            % Sets the flow rate to 0 which sets matter properties
+            this.update();
+            
+        end
+        
+    end
+    
+    methods (Access = protected)
+        
+        %% Update functions, called directly by timer
+        function update(this)
+
+            if this.oBranch.oTimer.fTime < 0
+                % If we are still constructing the simulation system (time
+                % is smaller than zero), do nothing except call the parent
+                % class update method.
+                update@solver.matter.base.branch(this, 0);
+                return;
+            end
+            
+            if this.oBranch.oTimer.fTime <= this.fLastUpdate
+                % If branch update has been called before during this time
+                % step, do nothing. 
+                return;
+            end
+            
+            % Actually compute the new flow rate and the associated delta
+            % pressures as well as delta temperatures.
+            [ fFlowRate, afDeltaP ] = this.solveFlowRate();
+            
+            % See base branch, same check here - if input phase nearly
+            % empty, just set flow rate to zero
+            oIn = this.oBranch.coExmes{sif(fFlowRate >= 0, 1, 2)}.oPhase;
+            
+            if tools.round.prec(oIn.fMass, oIn.oStore.oTimer.iPrecision) == 0
+                fFlowRate = 0;
+            end
+            
+            
+            update@solver.matter.base.branch(this, fFlowRate, afDeltaP);
+            
+        end
+    end
+    
+    
+    methods (Access = public)
+        
+        %% Solve branch
+        function [ fFlowRate, afDeltaP ] = solveFlowRate(this)
+            % Calculates flow rate for a branch. Flow rate fFlowRate here
+            % is NOT signed (negative/positive depending on direction, left
+            % to right is positive), therefore iDir value maintained.
+            % For solverDeltas, a positive pressure drop that is returned
+            % ALWAYS defines a pressure drop, a negative one a pressure
+            % rise, so for example a pipe, where the direction of the flow
+            % does not matter, can always return a positive value and just
+            % abs() the provided flow rate.
+            
+            oBranch = this.oBranch;
+            
+            %%% Old flow rate, pressure differences etc
+            fPreviousFlowRate = oBranch.fFlowRate;
+            
+            % Connected exmes
+            oExmeL = oBranch.coExmes{1};
+            oExmeR = oBranch.coExmes{2};
+            
+            % Calculating the pressure differences between the connected
+            % phases
+            [ fPressureLeft,  ~ ] = oExmeL.getPortProperties();
+            [ fPressureRight, ~ ] = oExmeR.getPortProperties();
+            
+            fPressureDifference = fPressureLeft - fPressureRight;
+            
+            if fPreviousFlowRate == 0
+                % previous flowrate was 0, check if another solution should
+                % apply
+                fFlowRate = 1e-8;
+                if fPressureDifference >= 0
+                    iDir = 1;
+                elseif fPressureDifference < 0
+                    iDir = -1;
+                end
+            else
+                iDir = sign(fPreviousFlowRate);
+                fFlowRate = abs(fPreviousFlowRate);
+            end
+            
+            [fError, fPressureRise, ~, mfData] = this.calculatePressureDrops(iDir, fFlowRate, fPressureDifference);
+            
+            if abs(fError) < this.fMaxError
+                % if the error is already smaller we have the solution
+                fFlowRate   = iDir * fFlowRate;
+                afDeltaP    = mfData(:, 1);
+                return
+            else
+                % Initial guesses for the other side of the intervall
+                fSecondError = fError;
+                fSecondFlowRate = fFlowRate;
+                iCounter = 0;
+                while sign(fError) == sign(fSecondError) && iCounter < this.iMaxIterations
+                    % we have to find another flowrate for which the error has
+                    % the opposite sign, the correct solution is in between
+                    % these two options
+                    if fError < 0
+                        % if the error is already smaller than 0, the pressure
+                        % drops are already larger than they should be and the
+                        % other side of the solution intervall is 0
+                        fSecondFlowRate = 0;
+
+                        [fSecondError, ~, ~, ~] = this.calculatePressureDrops(iDir, fSecondFlowRate, fPressureDifference);
+                        
+                    elseif sign(fPressureDifference + fPressureRise) ~= sign(iDir)
+                        % in this case the possible solution is at the
+                        % opposite flow direction than the initial guess
+                        iDir = -1 * iDir;
+                        
+                        % this is the only case where the initial interval
+                        % limitiation is changed!
+                        [fError, fPressureRise, ~, ~] = this.calculatePressureDrops(iDir, fFlowRate, fPressureDifference);
+                        fSecondError = fError;
+                    else
+                        % if the difference is larger than 0, the pressure rise
+                        % is larger than the pressure drops and an absolute
+                        % higher flowrate is necessary
+                        
+                        fSecondFlowRate = fSecondFlowRate * 10;
+                        
+                        [fSecondError, ~, ~, ~] = this.calculatePressureDrops(iDir, fSecondFlowRate, fPressureDifference);
+                        
+                    end
+                    
+                    iCounter = iCounter + 1;
+                end
+                
+                if iCounter == this.iMaxIterations
+                   this.throw('solveBranch', 'Too many iterations during interval definition');
+                end
+            end
+            
+            iCounter = 0;
+            if fFlowRate < fSecondFlowRate
+                fLowerBoundary = fFlowRate;
+                fUpperBoundary = fSecondFlowRate;
+            else
+                fLowerBoundary = fSecondFlowRate;
+                fUpperBoundary = fFlowRate;
+            end
+            
+            fNewError = fError;
+            
+            while (abs(fNewError) > this.fMaxError)  && iCounter < this.iMaxIterations
+                
+                % we split the initial intervall down in the middle
+                fIntervallSize = fUpperBoundary - fLowerBoundary;
+                
+                fNewFlowRate = fLowerBoundary + fIntervallSize/2;
+                
+                [fNewError, ~, ~, mfData] = this.calculatePressureDrops(iDir, fNewFlowRate, fPressureDifference);
+                
+                % if the error is negative, the pressure rise is higher
+                % than the drop, the drop increases with flowrate therefore
+                % the lower boundary is changed. In the other case the
+                % drops are higher than the pressure rises and therefore
+                % the upper boundary is moved
+                if fNewError > 0
+                    fLowerBoundary = fNewFlowRate;
+                elseif fNewError < 0
+                    fUpperBoundary = fNewFlowRate;
+                else
+                    % Error is 0, iteration concludes
+                end
+                
+                % Loop counter
+                iCounter = iCounter + 1;
+            end
+           
+            if iCounter == this.iMaxIterations
+               this.throw('solveBranch', 'Too many iterations during nested interval solution');
+            end
+            
+            fFlowRate   = iDir * fNewFlowRate;
+            afDeltaP    = mfData(:, 1);
+        end
+    end
+    methods (Access = protected)
+        function [fError, fPressureRise, fPressureDrop, mfData] = calculatePressureDrops(this, iDir, fFlowRate, fPressureDifference)
+            % This function is used internally to calculate the error and
+            % recalculate the pressure drops/rises in the f2fs
+            
+            % Data matrix - rows equals amount of flow procs minus two (the
+            % EXMEs), columns are the pressure drops and temperature
+            % changes returned by the components (f2f processors)
+            mfData = zeros(this.oBranch.iFlowProcs, 1);
+            
+            % calculate the pressure drops for the initial assumption
+            for iP = 1:this.oBranch.iFlowProcs
+                mfData(iP, 1) = this.aoSolverProps(iP).calculateDeltas(iDir * fFlowRate);
+            end
+            
+            % Calculate the pressure rise, drop and the solution error.
+            % Note that pressure rise and pressure difference are defined
+            % so that positive values result in a positive flow direction.
+            % If the sum of both values changes sign the flow direction
+            % must be changed!
+            if iDir > 0
+                fPressureRise   = -sum(mfData(mfData < 0));
+            else
+                fPressureRise   =  sum(mfData(mfData < 0));
+            end
+            fPressureDrop   =  sum(mfData(mfData > 0));
+            fError          =  abs(fPressureDifference + fPressureRise) - abs(fPressureDrop);
+        end
+    end
+end
--- user/+tutorials/+fan_loop_flow/setup.m
+++ user/+tutorials/+fan_loop_flow/setup.m
@@ -35,20 +35,54 @@ classdef setup < simulation.infrastructure
         end
         
         function configureMonitors(this)
-            %% Logging
-            tiLog = this.toMonitors.oLogger.add('Example', 'flow_props');
             
-            %% Plot definition
-            this.toMonitors.oPlotter.definePlotWithFilter(tiLog, 'Pa',   'Pressures')
-            this.toMonitors.oPlotter.definePlotWithFilter(tiLog, 'K',    'Temperatures')
-            this.toMonitors.oPlotter.definePlotWithFilter(tiLog, 'kg',   'Masses')
-            this.toMonitors.oPlotter.definePlotWithFilter(tiLog, 'kg/s', 'Flow Rates')
-
+            %% Logging
+            oLog = this.toMonitors.oLogger;
+            
+            csStores = fieldnames(this.oSimulationContainer.toChildren.Example.toStores);
+            for iStore = 1:length(csStores)
+                oLog.addValue(['Example.toStores.', csStores{iStore}, '.aoPhases(1)'],	'this.fMass * this.fMassToPressure',	'Pa', [csStores{iStore}, ' Pressure']);
+                oLog.addValue(['Example.toStores.', csStores{iStore}, '.aoPhases(1)'],	'fTemperature',	'K',  [csStores{iStore}, ' Temperature']);
+            end
+            
+            csBranches = fieldnames(this.oSimulationContainer.toChildren.Example.toBranches);
+            for iBranch = 1:length(csBranches)
+                oLog.addValue(['Example.toBranches.', csBranches{iBranch}],             'fFlowRate',    'kg/s', [csBranches{iBranch}, ' Flowrate']);
+            end
+            
         end
         
         function plot(this, varargin) % Plotting the results
-            this.toMonitors.oPlotter.plot(varargin{:});
             
+            %% Define Plots
+            
+            close all
+            oPlotter = plot@simulation.infrastructure(this);
+            
+            
+            csStores = fieldnames(this.oSimulationContainer.toChildren.Example.toStores);
+            csPressures = cell(length(csStores),1);
+            csTemperatures = cell(length(csStores),1);
+            for iStore = 1:length(csStores)
+                csPressures{iStore} = ['"', csStores{iStore}, ' Pressure"'];
+                csTemperatures{iStore} = ['"', csStores{iStore}, ' Temperature"'];
+            end
+            
+            csBranches = fieldnames(this.oSimulationContainer.toChildren.Example.toBranches);
+            csFlowRates = cell(length(csBranches),1);
+            for iBranch = 1:length(csBranches)
+                csFlowRates{iBranch} = ['"', csBranches{iBranch}, ' Flowrate"'];
+            end
+            
+            tPlotOptions.sTimeUnit = 'seconds';
+            tFigureOptions = struct('bTimePlot', false, 'bPlotTools', false);
+
+            coPlots{1,1} = oPlotter.definePlot(csPressures,     'Pressures', tPlotOptions);
+            coPlots{2,1} = oPlotter.definePlot(csFlowRates,     'Flow Rates', tPlotOptions);
+            coPlots{1,2} = oPlotter.definePlot(csTemperatures,  'Temperatures', tPlotOptions);
+            oPlotter.defineFigure(coPlots,  'Plots', tFigureOptions);
+            
+            oPlotter.plot();
         end
         
     end
