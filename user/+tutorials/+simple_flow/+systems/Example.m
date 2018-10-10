133c91bcbbef31bf2a67738333de5b1c9e43466b
Claas Olthoff <c.olthoff@tum.de>
2018-10-10 16:51:39 +0200

    TUTORIALS: Make simple_flow simple again! Threw out everything that is not directly necessary for the operation of this tutorial. Modified the comments accordingly. Changed to automatic exme creation.

--- user/+tutorials/+simple_flow/+systems/Example.m
+++ user/+tutorials/+simple_flow/+systems/Example.m
@@ -1,52 +1,35 @@
 classdef Example < vsys
     %EXAMPLE Example simulation for a simple flow in V-HAB 2.0
-    %   Two tanks filled with gas at different pressures and a pipe in between
+    %   Two tanks filled with gas at different temperatures and pressures
+    %   with a pipe in between
     
-    properties (SetAccess = protected, GetAccess = public)
-        fPipeLength   = 1.5;
-        fPipeDiameter = 0.005;
-        
-        % Pressure difference in bar
-        fPressureDifference = 1;
+    properties
+        % This system does not have any properties
     end
     
     methods
         function this = Example(oParent, sName)
-            % Call parent constructor. Third parameter defined how often
-            % the .exec() method of this subsystem is called. This can be
-            % used to change the system state, e.g. close valves or switch
-            % on/off components.
+            % Calling the parent constructor. This has to be done for any
+            % class that has a parent. The third parameter defines how
+            % often the .exec() method of this subsystem is called. 
             % Values can be: 0-inf for interval in [s] (zero means with
             % lowest time step set for the timer). -1 means with every TICK
             % of the timer, which is determined by the smallest time step
-            % of any of the systems. Providing a logical false (def) means
-            % the .exec method is called when the oParent.exec() is
-            % executed (see this .exec() method - always call exec@vsys as
-            % well!).
+            % of any of the systems. Providing a logical 'false' means the
+            % .exec() method is called when the oParent.exec() is executed
+            % (see this .exec() method - always call exec@vsys as well!).
             this@vsys(oParent, sName, 30);
-            
-            % Make the system configurable
-%             disp(this);
-%             disp('------------');
-%             disp(this.oRoot.oCfgParams.configCode(this));
-%             disp('------------');
-%             disp(this.oRoot.oCfgParams.get(this));
-%             disp('------------');
-            eval(this.oRoot.oCfgParams.configCode(this));
-            
-            %disp(this);
-            
             
         end
         
         
         function createMatterStructure(this)
+            % This function creates all simulation objects in the matter
+            % domain. 
+            
+            % First we always need to call the createMatterStructure()
+            % method of the parent class.
             createMatterStructure@vsys(this);
-            
-            % Atmos - Capacity
-            matter.store(this, 'Atmos', 100);
-            this.toStores.Atmos.createPhase('air', 'hell', 100, 300);
-            
             
             % Creating a store, volume 1 m^3
             matter.store(this, 'Tank_1', 1);
@@ -58,60 +41,73 @@ classdef Example < vsys
             matter.store(this, 'Tank_2', 1);
             
             % Adding a phase to the store 'Tank_2', 2 m^3 air at 50 deg C
-            oAirPhase = this.toStores.Tank_2.createPhase('air', this.fPressureDifference + 1, 323.15);
+            % Note that we are creating a phase that is twice as voluminous
+            % as the store it is in. This means that the pressure of this
+            % phase will be higher than that of 'Tank_1'
+            oAirPhase = this.toStores.Tank_2.createPhase('air', 2, 323.15);
             
-            % Adding extract/merge processors to the phase
-            matter.procs.exmes.gas(oGasPhase, 'Port_1');
-            matter.procs.exmes.gas(oAirPhase, 'Port_2');
-             
             % Adding a pipe to connect the tanks, 1.5 m long, 5 mm in
-            % diameter.
-            components.pipe(this, 'Pipe', this.fPipeLength, this.fPipeDiameter);
+            % diameter. The pipe is in the components library and is
+            % derived from the flow-to-flow (f2f) processor class
+            components.pipe(this, 'Pipe', 1.5, 0.005);
             
-            % Creating the flowpath (=branch) between the components
-            % Input parameter format is always: 
-            % 'store.exme', {'f2f-processor, 'f2fprocessor'}, 'store.exme'
-            matter.branch(this, 'Tank_1.Port_1', {'Pipe'}, 'Tank_2.Port_2', 'Branch');
+            % Creating the flowpath, called a branch in V-HAB, between the
+            % components. 
+            % The input parameter format is always:
+            % system object, phase object, {name(s) of f2f-processor(s)}, phase object
+            matter.branch(this, oGasPhase, {'Pipe'}, oAirPhase, 'Branch');
             
         end
         
         
         function createThermalStructure(this)
+            % This function creates all simulation objects in the thermal
+            % domain. 
+            
+            % First we always need to call the createThermalStructure()
+            % method of the parent class.
             createThermalStructure@vsys(this);
             
-%             oCapa = this.addCreateCapacity(this.toStores.Atmos.toPhases.hell);
-%             oProc = this.toProcsF2F.Pipe;
-%             fArea = oProc.fLength * pi * (oProc.fDiameter / 2)^2;
+            % We need to do nothing else here for this simple model. All
+            % thermal domain objects related to advective (mass-based) heat
+            % transfer will automatically be created by the
+            % setThermalSolvers() method. 
+            % Here one would create simulation objects for radiative and
+            % conductive heat transfer.
             
-            %TODO debug - if included, time step stuck at min
-            %thermal.f2f_wrapper(oProc, oCapa, 1, fArea);
         end
         
         
         function createSolverStructure(this)
+            % This function creates all of the solver objects required for
+            % a simulation. 
+            
+            % First we always need to call the createSolverStructure()
+            % method of the parent class.
             createSolverStructure@vsys(this);
             
-            % Now that the system is sealed, we can add the branch to a
-            % specific solver. In this case we will use the iterative
-            % solver. 
-            solver.matter.interval.branch(this.aoBranches(1));
-            
-            tTimeStepProperties.rMaxChange = 1e-3;
-            this.toStores.Tank_1.aoPhases(1).setTimeStepProperties(tTimeStepProperties);
-            this.toStores.Tank_2.aoPhases(1).setTimeStepProperties(tTimeStepProperties);
-            
+            % Creating an interval solver object that will solve for the
+            % flow rate of the one branch in this system.
+            solver.matter.interval.branch(this.toBranches.Branch);
+
+            % Since we want V-HAB to calculate the temperature changes in
+            % this system we call the setThermalSolvers() method of the
+            % thermal.container class. 
             this.setThermalSolvers();
+            
         end
     end
     
      methods (Access = protected)
         
         function exec(this, ~)
-            % exec(ute) function for this system
-            % Here it only calls its parent's exec function
+            % exec(ute) function for this system 
+            % This function can be used to change the system state, e.g.
+            % close valves or switch on/off components.
+            
+            % Here it only calls its parent's exec() method
             exec@vsys(this);
             
-            if ~base.oLog.bOff, this.out(2, 1, 'exec', 'Exec vsys %s', { this.sName }); end;
         end
         
      end
