35df9bc719bc3e3b5e8ac13a9d61531809f01824
Claas Olthoff <c.olthoff@tum.de>
2018-11-28 15:55:18 +0100

    CORE: Cleaned up the comments in the helper

--- core/+matter/+helper/+phase/+create/vacuum.m
+++ core/+matter/+helper/+phase/+create/vacuum.m
@@ -1,16 +1,14 @@
 function [ cParams, sDefaultPhase ] = vacuum(varargin)
 %VACUUM helper to create a vacuum matter phase
 
-% Create cParams for a whole matter.phases.gas standard phase. If user does
-% not want to use all of them, can just use
-% matter.phases.gas(oMT.create('air'){1:2}, ...)
+% Create cParams for a whole matter.phases.gas standard phase. Since the
+% vacuum is empty, the mass struct is empty as well. The volume is
+% infinite, the temperature is equal to the cosmic background of 3 K and
+% the pressure is zero. 
 cParams = { struct() Inf 3 0};
 
-
-% Default class - required for automatic construction of phase. Helper re-
-% turns the default phase that could be constructed with this set of params
+% Returning the phase constructor path that the parameters are to be used
+% on. 
 sDefaultPhase = 'matter.phases.boundary.gas';
-
-
 
 end
