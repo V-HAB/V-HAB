V-HAB/V-HAB

commit 8b30d6cc7620e9135923b371da7c63f0fa7bee7a

CORE: Added a initialization change for the multi solver in case that negative pressures for the flow phases are calculated. This should solve the issue for cases where the overall configuration is physically valid and only a step in the iteration was unstable. However, physically impossible systems like the first multi branch example are still not solveable with this

core/+solver/+matter_multibranch/+iterative/@branch/update.m
@@ -50,6 +50,7 @@ function update(this)
     % is repeated again until the solver reaches overall convergence
     % Initially true so that in the first tick the matrices are calculated!
     this.bFinalLoop = true;
+    bPressureError = false;
     
     % parameters necessary in case the P2P flowrates oscillate
     iP2PUpdates = 0;
@@ -60,7 +61,9 @@ function update(this)
     mfFlowRates = nan(this.iMaxIterations, this.iBranches);
     afP2PFlows  = nan(this.iMaxIterations, this.iBranches);
     
-    while abs(rError) > rErrorMax || this.bFinalLoop %|| iIteration < 5
+    this.fInitializationFlowRate = this.oTimer.fMinimumTimeStep;
+    
+    while abs(rError) > rErrorMax || this.bFinalLoop || bPressureError %|| iIteration < 5
         this.iIteration = this.iIteration + 1;
         
         % if we have reached convergence recalculate the p2ps
@@ -198,6 +201,8 @@ function update(this)
         
         warning('on','all');
         
+        
+        bPressureError = false;
         % translate the calculated results into branch flowrates or
         % gas flow node pressures
         for iColumn = 1:iNewRows
@@ -222,8 +227,25 @@ function update(this)
                     this.afFlowRates(iB) = (this.afFlowRates(iB) * 3 + afResults(iColumn)) / 4;
                 end
             elseif isa(oObj, 'matter.phases.flow.flow')
-                oObj.setPressure(afResults(iColumn));
+                if afResults(iColumn) < 0
+                    % This case occurs for example if a manual solver
+                    % flowrate is used as boundary condition and forces the
+                    % loop flowrates to high values, while the
+                    % initialization is at low flow rates
+                    bPressureError = true;
+                else
+                    oObj.setPressure(afResults(iColumn));
+                end
             end
+        end
+        
+        if bPressureError
+            this.fInitializationFlowRate = 10 * this.fInitializationFlowRate;
+            if this.fInitializationFlowRate > max(abs(afBoundaryConditions(iStartZeroSumEquations:end)))
+                this.fInitializationFlowRate = max(abs(afBoundaryConditions(iStartZeroSumEquations:end)));
+            end
+            
+            this.afFlowRates = afPrevFrs;
         end
         
         % for the branches which were removed beforehand because they have
@@ -248,7 +270,7 @@ function update(this)
         % otherwise it continues normally again
         if this.bFinalLoop && rError < this.fMaxError
             this.bFinalLoop = false;
-        elseif rError < this.fMaxError
+        elseif rError < this.fMaxError && ~bPressureError
             this.bFinalLoop = true;
         else
             this.bFinalLoop = false;

core/+solver/+matter_multibranch/+iterative/@branch/branch.m
@@ -317,6 +317,8 @@ classdef branch < base & event.source
         % the update level calculation
         mbExternalBoundaryBranches;
         iNumberOfExternalBoundaryBranches = 0;
+        
+        fInitializationFlowRate;
     end
     
     

core/+solver/+matter_multibranch/+iterative/@branch/updatePressureDropCoefficients.m
@@ -87,7 +87,7 @@ function [aafPhasePressuresAndFlowRates, afBoundaryConditions] = updatePressureD
             % aafPhasePressuresAndFlowRates matrix at the corresping index
             if fFlowRate == 0
                 % for no flowrate we check the drops with a very small flow
-                fFlowRate = this.oTimer.fMinimumTimeStep;
+                fFlowRate = this.fInitializationFlowRate;
                 
                 % Negative pressure difference? Negative guess!
                 if oB.coExmes{1}.getPortProperties() < oB.coExmes{2}.getPortProperties()

user/+examples/+multibranch_solver/+systems/Example.m
@@ -4,7 +4,7 @@ classdef Example < vsys
     
     properties (SetAccess = protected, GetAccess = public)
         fPipeLength   = 0.5;
-        fPipeDiameter = 0.001; %% for 3/5 mm overflow of warnings
+        fPipeDiameter = 0.01; %% for 3/5 mm overflow of warnings
         
         oManual;
     end

commit 875cba9278d4d8a706e37ce1b65546e0f32d872f

CORE: Removed the pressure error concept introduced in 8b30d6cc. It caused more problems than it solved. The better approach turned out to be to just ignore the negative pressure and keep the previous one. The solver error will still be above the cutoff threshold and after a few more iterations a better solution is usually found. In order to support debugging a local struct variable was introduced that stores the phases that would have had negative pressures assigned to them.

core/+solver/+matter_multibranch/+iterative/@branch/update.m
@@ -226,6 +226,8 @@ function update(this)
             this.throw('solver', 'NaNs in the Multi-Branch Solver Results!');
         end
         
+        toPhasesWithNegativePressures = struct();
+        
         % translate the calculated results into branch flowrates or
         % gas flow node pressures
         for iColumn = 1:iNewRows
@@ -254,21 +256,15 @@ function update(this)
                     % This case occurs for example if a manual solver
                     % flowrate is used as boundary condition and forces the
                     % loop flowrates to high values, while the
-                    % initialization is at low flow rates
-                    bPressureError = true;
+                    % initialization is at low flow rates. We ignore this
+                    % value here and hopefully the solver will calculate
+                    % something better in the next iteration. Just in case,
+                    % we record the objects here to help with debugging. 
+                    toPhasesWithNegativePressures.(oObj.sUUID) = oObj;
                 else
                     oObj.setPressure(afResults(iColumn));
                 end
             end
-        end
-        
-        if bPressureError
-            this.fInitializationFlowRate = 10 * this.fInitializationFlowRate;
-            if this.fInitializationFlowRate > max(abs(afBoundaryConditions(iStartZeroSumEquations:end)))
-                this.fInitializationFlowRate = max(abs(afBoundaryConditions(iStartZeroSumEquations:end)));
-            end
-            
-            this.afFlowRates = afPrevFrs;
         end
         
         % For the branches which were removed beforehand because they have