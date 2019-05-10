c2da45670c0f998ffb016933d42a694909121d07
Claas Olthoff <c.olthoff@tum.de>
2019-05-10 14:14:39 -0500

    LIB: Breaking change of valve component. Remove flow coefficient. Valve is now 'dumb', has no resistance when open and infinite resistance when closed. Detailed model will be added at a later date. The RCA and CDRA models both use this components, so they were changed accordingly. CDRA was tested using example case. RCA example is currently inoperable, so no test was performed.

--- lib/+components/+matter/valve.m
+++ lib/+components/+matter/valve.m
@@ -1,29 +1,25 @@
 classdef valve < matter.procs.f2f
-    % this is a simple closable valve, which can open and close branches.
+    % this is a simple valve, which can open and close branches.
     % Works with interval and multibranch iterative solver
     
     properties
         % Valve open or closed?
         bOpen = true;
-        fFlowCoefficient = 1;
     end
     
     methods
-        function  this = valve(oContainer, sName, fFlowCoefficient, bOpen)
+        function  this = valve(oContainer, sName, bOpen)
             % Input parameters:
-            %   sName:              name of the valve [char]
-            %   fFlowCoefficient:   
-            %   bOpen:              inital value of the valve setting [boolean]
+            %   sName: name of the valve [char]
+            %   bOpen: inital value of the valve setting [boolean]
             
             this@matter.procs.f2f(oContainer, sName);
             
-            this.fFlowCoefficient   = fFlowCoefficient;
-            
-            if nargin >= 4 && ~isempty(bOpen) && islogical(bOpen)
+            if nargin >= 3 && ~isempty(bOpen) && islogical(bOpen)
                 this.bOpen = ~~bOpen;
             end
             
-            if bValveOpen
+            if this.bOpen
                 this.fDeltaPressure = 0;
             else
                 this.fDeltaPressure = Inf;
@@ -42,50 +38,16 @@ classdef valve < matter.procs.f2f
             end
         end
         
-        function fDeltaPress = solverDeltas(this, fFlowRate)
-            
-            if (this.fFlowCoefficient == 0) && this.bOpen
-                fDeltaPress = 0;
-                this.fDeltaPressure = fDeltaPress;
-                return;
-            end
-            
-            if (fFlowRate == 0)
-                fDeltaPress = 0;
-                this.fDeltaPressure = fDeltaPress;
-                return;
-            end
+        function fDeltaPress = solverDeltas(this, ~)
             
             if this.bOpen == 0
                 fDeltaPress = Inf;
-                return;
-            end
-            
-            
-            % Get in/out flow object references
-            [ oFlowIn, oFlowOut ] = this.getFlows(fFlowRate);
-            
-            % Average pressure in Pa
-            fP = (oFlowIn.fPressure + oFlowOut.fPressure) / 2;
-            
-            % No pressure at all ... normally just return, drop zero
-            if fP == 0
+                this.fDeltaPressure = Inf;
+            else
                 fDeltaPress = 0;
-                return;
+                this.fDeltaPressure = 0;
             end
             
-            % Calculate density in kg/m^3 and flow rate in SLM
-            fRoh = (fP * oFlowIn.fMolarMass) / (this.oMT.Const.fUniversalGas * oFlowIn.fTemperature);
-            fFlowRate = fFlowRate * 60; % Convert to kg/min
-            fSLM = (fFlowRate / oFlowIn.fMolarMass * ...
-                   this.oMT.Const.fUniversalGas * oFlowIn.fTemperature / oFlowIn.fPressure) * 1000;
-
-               
-            % Calculate Pressure Difference in 
-            fDeltaPress =  fSLM^2 / (fRoh * this.fFlowCoefficient^2);  
-            
-            
-            %this.fDeltaPressure = fDeltaPress;
         end
         
         function this = setOpen(this, bOpen)
