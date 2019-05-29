bf2d817bdc91fecc130cce71a98a59af4548b215
Daniel Pütz <d.puetz@tum.de>
2019-05-29 10:31:16 +0200

    CORE: added a private function to the matter table that can be used by the calculateXXX functions to generate the parameter set. Previously this was done in each function individually. TBD: The calculatePressure function has additional functionality built in, it is currently a bit unclear how that should be handled

--- core/+matter/@table/calculateSpeedOfSound.m
+++ core/+matter/@table/calculateSpeedOfSound.m
@@ -1,91 +1,19 @@
-function fSpeedOfSound = calculateSpeedOfSound(this, varargin) %sMatterState, afMasses, fTemperature, fPressure)
-    %CALCULATESPECIFICHEATCAPACITY Calculate the specific heat capacity of a mixture
-    %    Calculates the specific heat capacity by adding the single
-    %    substance capacities weighted with their mass fraction. Can use
+function fSpeedOfSound = calculateSpeedOfSound(this, varargin)
+    %calculateSpeedOfSound: Calculate the speed of sound for provided matter
+    %    Calculates the speed of sound by adding the single
+    %    substance speed of sounds weighted with their mass fraction. Can use
     %    either a phase object as input parameter, or the phase type
     %    (sType) and the masses array (afMasses). Optionally, temperature
     %    and pressure can be passed as third and fourth parameters,
     %    respectively.
     %
-    % calculateHeatCapacity returns
-    %  fSpecificHeatCapacity  - specific, isobaric heat capacity of mix in J/kgK?
-    %
-    %TODO: deprecate this method in favor of |getSpecificHeatCapacity()|
-    %      and |getTotalHeatCapacity()|, which just handle the second case
-
-    iNumArgs = length(varargin); % |iNumArgs == nargin - 1|!?
-
-    % Handle two variants of calling this method: With an object, where the
-    % necessary data can be retrieved from, or the data itself.
-    if iNumArgs == 1 %nargin < 3
-        % First case: Just a phase or flow object is provided.
-        %TODO: Delete this part and put it into the corresponding classes
-        %      instead (the matter table should not know about other objects).
-
-        oMatterRef = varargin{1};
-
-        % Get data from object: The state of matter (gas, liquid, solid)
-        % and |afMasses| array, depending on the object type.
-        if isa(oMatterRef, 'matter.phase')
-            sMatterState = oMatterRef.sType;
-            afMasses     = oMatterRef.afMass;
-        elseif isa(oMatterRef, 'matter.procs.p2p')
-            sMatterState = oMatterRef.getInEXME().oPhase.sType;
-            afMasses     = oMatterRef.arPartialMass * oMatterRef.fFlowRate;
-            % Because the flow rate can be negative, we transform the
-            % afMasses array into absolutes here. Otherwise the calculated
-            % heat capacity will be zero.
-            afMasses = abs(afMasses);
-        elseif isa(oMatterRef, 'matter.flow')
-            sMatterState = oMatterRef.oBranch.getInEXME().oPhase.sType;
-            afMasses     = oMatterRef.arPartialMass * oMatterRef.fFlowRate;
-            % Because the flow rate can be negative, we transform the
-            % afMasses array into absolutes here. Otherwise the calculated
-            % heat capacity will be zero.
-            afMasses = abs(afMasses);
-
-        else
-            this.throw('calculateHeatCapacity', 'Single parameter must be of type |matter.phase| or |matter.flow|.');
-        end
-
-        % Get data from object: Temperature and pressure.
-        fTemperature = oMatterRef.fTemperature;
-        fPressure    = oMatterRef.fPressure;
-
-        if isempty(fPressure) || isnan(fPressure)
-            fPressure = this.Standard.Pressure; % std pressure (Pa)
-        end
-
-        if isempty(fTemperature) || isnan(fTemperature)
-            fTemperature = this.Standard.Temperature; % std temperature (K)
-        end
-    else
-        % Second case: Data is provided directly.
-
-        sMatterState  = varargin{1}; % solid, liquid, or gas
-        afMasses      = varargin{2}; % mass per substance (array)
-        
-        % Get temperature and pressure from arguments, otherwise use
-        % standard data.
-        if iNumArgs > 2
-            fTemperature = varargin{3};
-        else
-            fTemperature = this.Standard.Temperature; % std temperature (K)
-        end
-        
-        if iNumArgs == 4
-            fPressure    = varargin{4};
-        else
-            fPressure    = this.Standard.Pressure;    % std pressure (Pa)
-        end
-        
-        if iNumArgs > 4
-            fDensity   = varargin{4};
-        end
-    end
+    % calculateSpeedOfSound returns
+    %  fSpeedOfSound  - speed of sound in m/s
+    
+    [fTemperature, arPartialMass, csPhase, aiPhase, aiIndices, ~, ~, ~] = getNecessaryParameters(this, varargin{:});
 
     % If no mass is given, the heat capacity is also zero.
-    if sum(afMasses) == 0
+    if sum(arPartialMass) == 0
         fSpeedOfSound = 0;
         return; % Return early.
     end
@@ -98,21 +26,12 @@ function fSpeedOfSound = calculateSpeedOfSound(this, varargin) %sMatterState, af
     
     % Find substances with a mass bigger than zero and count the results.
     % This helps in getting only the needed data from the matter table.
-    aiIndices   = find(arPartialMass > 0);
     iNumIndices = length(aiIndices);
 
     % Initialize a new array filled with zeros. Then iterate through all
     % indexed substances and get their specific heat capacity.
     afSpeedOfSound = zeros(iNumIndices, 1);
     
-    csPhase = {'solid';'liquid';'gas';'supercritical'};
-    tiP2N.solid = 1;
-    tiP2N.liquid = 2;
-    tiP2N.gas = 3;
-    tiP2N.supercritical = 4;
-    if ~strcmp(sMatterState, 'mixture')
-        aiPhase = tiP2N.(sMatterState)*ones(1,this.iSubstances);
-    end
     for iI = 1:iNumIndices
         % Creating the input struct for the findProperty() method
         tParameters = struct();
@@ -133,30 +52,18 @@ function fSpeedOfSound = calculateSpeedOfSound(this, varargin) %sMatterState, af
         afSpeedOfSound(iI) = this.findProperty(tParameters);
     end
 
-    % Make sure there is no NaN in the specific heat capacity vector.
+    % Make sure there is no NaN in the speed of sound vector.
     assert(~any(isnan(afSpeedOfSound)), 'Invalid entries in specific heat capacity vector.');
 
     %DEBUG
     assert(isequal(size(afSpeedOfSound), size(arPartialMass(aiIndices)')), 'Vectors must be of same length but one transposed.');
 
-    % Multiply the specific heat capacities with the mass fractions. The
-    % result of the matrix multiplication is the specific heat capacity of
+    % Multiply the speed of sound with the mass fractions. The
+    % result of the matrix multiplication is the speed of sound of
     % the mixture.
     fSpeedOfSound = sum(arPartialMass(aiIndices)' .* afSpeedOfSound);
 
-    % Make sure the heat capacity value is valid.
+    % Make sure the speed of sound value is valid.
     assert(~isnan(fSpeedOfSound) && fSpeedOfSound >= 0, ...
         'Invalid speed of sound: %f', fSpeedOfSound);
-
-    % "Most physical systems exhibit a positive heat capacity. However,
-    % there are some systems for which the heat capacity is negative. These
-    % are inhomogeneous systems which do not meet the strict definition of
-    % thermodynamic equilibrium.
-    % A more extreme version of this occurs with black holes. According to
-    % black hole thermodynamics, the more mass and energy a black hole
-    % absorbs, the colder it becomes. In contrast, if it is a net emitter
-    % of energy, through Hawking radiation, it will become hotter and
-    % hotter until it boils away."
-    %     -- http://en.wikipedia.org/wiki/Heat_capacity 
-    %        (Retrieved: 2015-05-27 23:48 CEST)
 end
---
+++ core/+matter/@table/private/getNecessaryParameters.m
@@ -0,0 +1,143 @@
+function [fTemperature, arPartialMass, csPhase, aiPhase, aiIndices, tbReference, afPartialPressures, sMatterState] = getNecessaryParameters(this, varargin)
+% Case one - just a phase or flow object provided
+tbReference.bPhase = false;
+tbReference.bFlow = false;
+tbReference.bNone = false;
+
+if length(varargin) == 1
+    if ~isa(varargin{1}, 'matter.phase') && ~isa(varargin{1}, 'matter.flow')
+        this.throw('calculateDensity', 'If only one param provided, has to be a matter.phase or matter.flow (derivative)');
+    end
+    
+    % Initialize attributes from input object
+    % Getting the phase type (gas, liquid, solid) depending on the object
+    % type
+    if isa(varargin{1}, 'matter.phase')
+        sMatterState = varargin{1}.sType;
+        oPhase = varargin{1};
+        tbReference.bPhase = true;
+        
+    elseif isa(varargin{1}, 'matter.flow')
+        sMatterState = varargin{1}.oBranch.getInEXME().oPhase.sType;
+        oPhase = varargin{1}.oBranch.getInEXME().oPhase;
+        tbReference.bFlow = true;
+        
+    end
+    
+    fTemperature = varargin{1}.fTemperature;
+    arPartialMass = varargin{1}.arPartialMass;
+    fPressure = varargin{1}.fPressure;
+    
+    if strcmp(sMatterState, 'gas')
+        
+        [ afPartialPressures, ~ ] = this.calculatePartialPressures(varargin{1});
+        
+    elseif strcmp(sMatterState, 'liquid')
+        % If the pressure of the flow is zero, as would happen
+        % during initialization, we use the pressure of the phase. 
+        if fPressure == 0
+            afPartialPressures = ones(1, this.iSubstances) * oPhase.fPressure;
+        else
+            afPartialPressures = ones(1, this.iSubstances) * fPressure;
+        end
+    elseif strcmp(sMatterState, 'mixture')
+        % for mixtures the actual matter type is set by the user and
+        % also differs for each substance. The partial pressure for a gas
+        % mixture phase (e.g. gas that contains solids) has to be
+        % calculated the same way as for a gas phase except for the
+        % substances that are solid
+
+        if isempty(oPhase.sPhaseType)
+            afPartialPressures = ones(1,this.iSubstances) .* this.Standard.Pressure;
+            aiPhase = this.determinePhase(arPartialMass, fTemperature, ones(1,this.iSubstances) .* this.Standard.Pressure);
+        else
+            aiPhase = this.determinePhase(arPartialMass, fTemperature, ones(1,this.iSubstances) .* fPressure);
+            if strcmp(oPhase.sPhaseType, 'gas')
+                afMassGas = zeros(1,this.iSubstances);
+                afMassGas(aiPhase ~= 1) = oPhase.afMass(aiPhase ~= 1);
+                afPartialPressures = this.calculatePartialPressures('gas',afMassGas, fPressure, fTemperature);
+                afPartialPressures(aiPhase == 1) = fPressure;
+
+                aiPhase = this.determinePhase(arPartialMass, fTemperature, afPartialPressures);
+                
+                aiPhase = round(aiPhase,0);
+            else
+                afPartialPressures = ones(1,this.iSubstances) .* fPressure;
+            end
+        end
+    else
+        % For Solids we also calculate matter properties based on the
+        % overall pressure
+        if fPressure == 0
+            afPartialPressures = ones(1, this.iSubstances) * oPhase.fPressure;
+        else
+            afPartialPressures = ones(1, this.iSubstances) * fPressure;
+        end
+    end
+    
+    if isempty(fTemperature) || isnan(fTemperature)
+        fTemperature = this.Standard.Temperature; % std temperature (K)
+    end
+    
+else
+    % This part is in case values directly passed to this function, rather
+    % than a phase or flow object.
+    sMatterState = varargin{1};
+    xfMass       = varargin{2};
+    tbReference.bNone = true;
+    
+    if isstruct(xfMass)
+        tfMass = xfMass;
+        afMass = zeros(1, this.iSubstances);
+        csSubstances = fieldnames(tfMass);
+        for iSubstance = 1:length(csSubstances)
+            afMass(this.tiN2I.(csSubstances{iSubstance})) = tfMass.(csSubstances{iSubstance});
+        end
+    else
+        afMass = xfMass;
+    end
+    if sum(afMass) == 0
+        arPartialMass = zeros(1, this.iSubstances);
+    else
+        arPartialMass = afMass ./ sum(afMass);
+    end
+    
+    % if additional temperature and pressure given
+    if nargin > 3
+        fTemperature = varargin{3};
+    else
+        fTemperature = this.Standard.Temperature; % std temperature (K)
+    end
+    
+    % Assume if vector with length > 1 -> partials, else total pressure
+    if nargin > 4 && ~isempty(varargin{4}) && isvector(varargin{4}) && length(varargin{4}) > 1
+        afPartialPressures = varargin{4};
+    else
+        fPressure = this.Standard.Pressure;
+        
+        if nargin > 4 && ~isempty(varargin{4}) && isvector(varargin{4}) && isscalar(varargin{4})
+            fPressure = varargin{4};
+        end
+        
+        if any(strcmp(sMatterState, {'gas'}))
+            [ afPartialPressures, ~ ] = this.calculatePartialPressures(sMatterState, afMass, fPressure, fTemperature);
+        else
+            afPartialPressures = ones(1, this.iSubstances) * fPressure;
+        end
+    end
+    
+end
+
+% Find the indices of all substances that are in the flow
+aiIndices = find(arPartialMass > 0);
+
+csPhase = {'solid';'liquid';'gas';'supercritical'};
+tiP2N.solid = 1;
+tiP2N.liquid = 2;
+tiP2N.gas = 3;
+tiP2N.supercritical = 4;
+if ~strcmp(sMatterState, 'mixture')
+    aiPhase = tiP2N.(sMatterState)*ones(1,this.iSubstances);
+end
+
+end
