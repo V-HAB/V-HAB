c843239673c425bebe8e372f8abe1018dfb0812f
Claas Olthoff <c.olthoff@tum.de>
2019-09-24 16:24:47 -0500

    LIBRARY: Updated SWME component Now this component uses the new thermal framework and its dimensions match the 4th generation SWME.

--- lib/+components/+matter/+SWME/+stores/SWMEStore.m
+++ lib/+components/+matter/+SWME/+stores/SWMEStore.m
@@ -3,32 +3,75 @@ classdef SWMEStore < matter.store
     % connected with each other through the X50Membrane P2P processor
     
     properties
-        
     end
     
     methods
         
-        function this = SWMEStore(oContainer, sName, fSWMEVolume, fSWMEVaporVolume, fInitialTemperature)
+        function this = SWMEStore(oContainer, sName)
             
+            % Parsing the overall SWME vapor volume from the parent system.
+            fSWMEVaporVolume = oContainer.fSWMEVaporVolume;
+            
+            % Defining volume available for the liquid phase inside the
+            % hollow fibers
+            fSWMELiquidVolume = pi * (oContainer.fFiberInnerDiameter/2)^2 * oContainer.fFiberExposedLength * oContainer.iNumberOfFibers;
+            
+            fSWMEVolume = fSWMEVaporVolume + fSWMELiquidVolume;
+            
+            % Calling the parent class constructor
             this@matter.store(oContainer, sName, fSWMEVolume);
             
+            % Creating the input struct for the findProperty() method
+            tParameters = struct();
+            tParameters.sSubstance       = 'H2O';
+            tParameters.sProperty        = 'Density';
+            tParameters.sFirstDepName    = 'Temperature';
+            tParameters.fFirstDepValue   = oContainer.fInitialTemperature;
+            tParameters.sPhaseType       = 'liquid';
+            tParameters.sSecondDepName   = 'Pressure';
+            tParameters.fSecondDepValue  = 28300;
+            tParameters.bUseIsobaricData = true;
+            
+            fWaterDensity = this.oMT.findProperty(tParameters);
+            
+            fWaterMass = fWaterDensity * fSWMELiquidVolume;
+            
             % Creating liquid water phase inside the hollow fibers of the
-            % X50 membrane
+            % X50 membrane 
             oLiquidHoFiPhase = matter.phases.liquid(...
-                               this,...                      % Store where the phase is located
-                              'FlowPhase', ...               % Phase name
-                               struct('H2O', 0.0881439), ... % Phase contents
-                               fInitialTemperature,...       % Phase temperature
-                               28300);                       % Phase pressure
+                               this,...                           % Store where the phase is located
+                              'FlowPhase', ...                    % Phase name
+                               struct('H2O', fWaterMass), ...     % Phase contents
+                               oContainer.fInitialTemperature,... % Phase temperature
+                               28300);                            % Phase pressure
+            
+                           
+            % Calculating the mass of water we want to put into the SWME
+            % volume so it is in equilibrium with the hollow fibers at the
+            % initial temperature and with a closed BPV.
+            
+            % Calculating the mean saturation pressure inside the hollow
+            % fibers in [Pa]
+            fVaporPressure = this.oMT.calculateVaporPressure(oContainer.fInitialTemperature, 'H2O');
+            
+            % Updating the input struct for the findProperty() method
+            tParameters.sPhaseType      = 'gas';
+            tParameters.fSecondDepValue = fVaporPressure;
+            
+            % Now we can calculate the density based on the parameters
+            fVaporDensity = this.oMT.findProperty(tParameters);
+                           
+            % Calculating the water vapor mass
+            fVaporMass = fVaporDensity * fSWMEVaporVolume;
             
             % Creating the vapor phase filling the SWME around the hollow
             % fibers
             oVaporSWME = matter.phases.gas(...
                          this, ...                           % Store in which the phase is located
                         'VaporPhase', ...                    % Phase name
-                         struct('H2O', 3e-6), ...            % Phase contents
+                         struct('H2O', fVaporMass), ...      % Phase contents
                          fSWMEVaporVolume, ...               % Phase volume
-                         fInitialTemperature);               % Phase temperature
+                         oContainer.fInitialTemperature);    % Phase temperature
             
             % Creating exmes for the vapor phase
             matter.procs.exmes.gas(oVaporSWME, 'VaporIn');                % vapor exiting the  X50 membrane
@@ -39,17 +82,16 @@ classdef SWMEStore < matter.store
             matter.procs.exmes.liquid(oLiquidHoFiPhase, 'WaterOut');      % water exiting the SWME to the outlet feed tank
             matter.procs.exmes.liquid(oLiquidHoFiPhase, 'WaterToVapor');  % water evaporating through the membrane wall
             
+            % Calculating the total membrane area based on the static
+            % properties.
+            fAreaPerFiber = oContainer.fFiberExposedLength * 2 * pi * oContainer.fFiberInnerDiameter / 2;
+            fMembraneArea = fAreaPerFiber * oContainer.iNumberOfFibers; %#ok<NASGU>
+            
             % Creating P2P processor which describes the vapor flux from
             % the inside of the hollow fibers, through the hydrophobic
             % membrane wall, to the inside of the SWME housing
-            components.matter.SWME.procs.X50Membrane(this, 'X50Membrane', 'FlowPhase.WaterToVapor', 'VaporPhase.VaporIn');
-            
-        end
-        
-        function setTemperatureProcessor(this, oProcessor)
-            this.toProcsP2P.X50Membrane.setTemperatureProcessor(oProcessor);
+            eval([this.oMeta.ContainingPackage.ContainingPackage.Name, '.procs.X50Membrane(this, ''X50Membrane'', ''FlowPhase.WaterToVapor'', ''VaporPhase.VaporIn'', fMembraneArea);']);
         end
     end
-    
 end
 
