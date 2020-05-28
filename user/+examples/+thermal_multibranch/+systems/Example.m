5dca572ed490238776e8b9b1141be8578b36199a
Claas Olthoff <c.olthoff@tum.de>
2020-05-28 16:16:23 -0500

    EXAMPLES: Changed the thermal multi-branch example to include changing the heat flow on heat sources during run time. This is done to catch the error that was fixed in 6f98edf0. In the process cleaned up and added comments to the class file and extended the run time from 900 to 1800 seconds. Changed the animation to use smaller time steps and added a clock. The temperatures seem to drop faster than they rise, need to check if this is the actual behavior or a simulation error.

--- user/+examples/+thermal_multibranch/+systems/Example.m
+++ user/+examples/+thermal_multibranch/+systems/Example.m
@@ -1,74 +1,144 @@
 classdef Example < vsys
-    % an example of a 3-dimensional thermal problem, a cube which is
-    % located on space and has a internal heat source. The thermal multi
-    % branch is used to solve this problem efficiently and this example
-    % also serves as test case for the solver
+    %EXAMPLE Example thermal system
+    % This is an example of a 3-dimensional thermal problem, a cube which
+    % is located in space and has an internal heat source. The thermal
+    % multi-branch solver is used to solve this problem efficiently. This
+    % example also serves as test case for the solver. 
+    % Using the bAdvancedThermalSolver property, the user can switch
+    % between using the basic and advanced thermal multi-branch solvers.
+    % The default is using the advanced solver. 
     
     properties (SetAccess = protected, GetAccess = public)
+        % Volume of the cube that is modeled
+        fTotalCubeVolume = 1;
         
-        fTotalCubeVolume    = 1;
-        iNodesPerDirection  = 3;
-        fHeatFlow           = 100;
+        % Number of nodes along each side of the cube
+        iNodesPerDirection = 7;
+        
+        % Heat flow from the heatsource [W]
+        fHeatFlow = 500;
+        
+        % Initial temperature of all nodes [K]
+        fInitialTemperature = 200;
             
+        % A cell containing references to all nodes (capacities) in the
+        % system. We could use the coCapacities property of the thermal
+        % container for this, but by using our own property here, we can
+        % arrange the nodes in a three dimensional cell array, just like
+        % they are geometrically. 
         coNodes;
         
+        % A boolean switch that is used to determine if this uses the
+        % advanced thermal network solver or the simple one. The advanced
+        % solver calculates heat flows and capacity temperatures, the
+        % simple solver only calculates heat flows. 
         bAdvancedThermalSolver = true;
+        
+        % We want to vary the heat load during run time to create some
+        % dynamics, so we need this array of heat source objects to access
+        % them during the exec() call. 
+        aoHeatSources;
     end
     
     methods
         function this = Example(oParent, sName)
-            this@vsys(oParent, sName, inf);
+            % Calling the parent class constructor. The third argument will
+            % cause this object's exec() method to be executed every 120
+            % seconds. 
+            this@vsys(oParent, sName, 120);
             
+            % By default we initialize a 1 m^3 Cube of Aluminum, which has
+            % 7 thermal nodes as phases (which are also capacities), and
+            % automatically create the corresponding thermal branches. The
+            % minimum discretization is 3x3x3, so one node without
+            % connection to vaccum always exists. For thermal exchange at
+            % the space boundary, we assume radiative exchange with a space
+            % node. As thermal energy source, a 500 W energy source in the
+            % middle of the cube is modelled.
+            
+            % Setting the default values.
+            this.fTotalCubeVolume    = 1;
+            this.iNodesPerDirection  = 7;
+            this.fHeatFlow           = 500;
+            this.fInitialTemperature = 200;
+            
+            % By calling the following line, we make this system
+            % configurable. By passing a containers.Map variable to the
+            % setup function, we can set any of the properties of this
+            % object, like overwriting the defaults defined above. 
             eval(this.oRoot.oCfgParams.configCode(this));
             
+            % If the number of nodes is less than three, we can't have an
+            % internal node with the heat source attached. So we check for
+            % this condition and throw an error if it is not met. 
+            if this.iNodesPerDirection < 3
+                error('VHAB:Examples:NotEnoughNodes','define at least a 3x3x3 cube!')
+            end
         end
         
         
         function createMatterStructure(this)
+            % Calling this method in the parent class, that will also call
+            % it on all children of this object.
             createMatterStructure@vsys(this);
             
-            % Create Environment Capacity
+            % Creating the space store.
             matter.store(this, 'Space', Inf);
             
-            oSpace = this.toStores.Space.createPhase(  'gas', 'boundary',   'Vacuum',   1e6,    struct('N2', 2),          3,          0);
+            % Creating the space phase as a boundary phase. That means it
+            % will not change its composition or pressure and its
+            % associated thermal capacity will not change its temperature.
+            oSpace = this.toStores.Space.createPhase('gas','boundary','Vacuum', 1e6, struct('N2', 2), 3, 0);
+            
+            % If the advanced solver is selected, the thermal capacity that
+            % will be automatically created for the space phase needs to be
+            % an instance of thermal.capacities.network. If this is set to
+            % false, an instance of the "normal" thermal.capacity will be
+            % created. 
             if this.bAdvancedThermalSolver
                 oSpace.makeThermalNetworkNode();
             end
-            % For the Multibranch solver, we initialize a 1 m^3 Cube of
-            % Aluminium, which has a discretizable number of thermal nodes
-            % as phases (which are also capacities), and automatically
-            % creates the corresponding thermal branches. The minimum
-            % discretization is 3x3x3, so that one node without connection
-            % to the vaccum exists. For thermal exchange at the space
-            % boundary, we assume radiative exchange with the created space
-            % node. As thermal energy source, a 100 W energy source in the
-            % middle of the cube is assumed
             
-            % Variable Inputs the user can change:
-            this.fTotalCubeVolume    = 1;
-            this.iNodesPerDirection  = 7;
-            this.fHeatFlow           = 500;
-            fInitialTemperature      = 200;
-            
-            %% Remaining code
+            % Creating the store that will contain all of the phases that
+            % make up the cube. 
             matter.store(this, 'Cube', this.fTotalCubeVolume + 1e-3);
             
+            % Calculating the total number of nodes.
             iTotalNodes = this.iNodesPerDirection^3;
-            if this.iNodesPerDirection < 3
-                error('define at least a 3x3x3 cube!')
-            end
             
+            % Initializing the coNodes cell.
             this.coNodes = cell(this.iNodesPerDirection, this.iNodesPerDirection, this.iNodesPerDirection);
             
             % Now we create the nodes to discretize the cube
             for iX = 1:this.iNodesPerDirection
                 for iY = 1:this.iNodesPerDirection
                     for iZ = 1:this.iNodesPerDirection
+                        % Creating a node using the createPhase() method on
+                        % the store and the 'solid' helper. Note that the
+                        % 'solid' helper uses a mass ratio rather than
+                        % absolute masses in the mass struct. 
+                        oNode = this.toStores.Cube.createPhase(    ...
+                                'solid',                           ... Phase type
+                                ['Node_X', num2str(iX),            ... Phase name
+                                     '_Y', num2str(iY),            ...     "
+                                     '_Z', num2str(iZ)],           ...     "
+                                this.fTotalCubeVolume/iTotalNodes, ... Phase volume
+                                struct('Al', 1),                   ... Phase constituents and mass ratio
+                                this.fInitialTemperature,          ... Phase temperature
+                                1e5);                                % Phase pressure
                         
-                        oNode = this.toStores.Cube.createPhase(  'solid',   ['Node_X', num2str(iX),'_Y', num2str(iY),'_Z', num2str(iZ)],   this.fTotalCubeVolume/iTotalNodes,    struct('Al', 1),          fInitialTemperature,          1e5);
+                        % Same as above, if the advanced solver is
+                        % selected, the thermal capacity that will be
+                        % automatically created for the space phase needs
+                        % to be an instance of thermal.capacities.network.
+                        % If this is set to false, an instance of the
+                        % "normal" thermal.capacity will be created.
                         if this.bAdvancedThermalSolver
                             oNode.makeThermalNetworkNode();
                         end
+                        
+                        % Populating the coNodes cell array with the node
+                        % we just created. 
                         this.coNodes{iX, iY, iZ} = oNode;
                     end
                 end
@@ -78,24 +148,40 @@ classdef Example < vsys
         
         
         function createThermalStructure(this)
+            % Calling this method in the parent class, that will also call
+            % it on all children of this object.
             createThermalStructure@vsys(this);
             
+            % Getting a reference to the capacity associated with the Space
+            % phase that has been automatically created for us. 
             oSpaceCapacity = this.toStores.Space.toPhases.Vacuum.oCapacity;
             
+            % Calculating the length of a side of the cube. We need this
+            % value to calculate the surface area of each face. 
             fTotalCubeSideLength = this.fTotalCubeVolume^(1/3);
             
             %% Define the radiative resistance of each outfacing side
-            fEpsilon        = 0.8;
-            fSightFactor    = 1;
-            % The area of each node facing to one of its sides
-            fNodeArea     	= (fTotalCubeSideLength / this.iNodesPerDirection)^2;
-            fRadiativeResistance = 1 / (fEpsilon * fSightFactor * this.oMT.Const.fStefanBoltzmann * fNodeArea);
+            fEpsilon    = 0.8;
+            fViewFactor = 1;
+            
+            % The area of each node face
+            fNodeArea = (fTotalCubeSideLength / this.iNodesPerDirection)^2;
+            
+            % Radiative resistance between each outside node and the space
+            % capacity
+            fRadiativeResistance = 1 / (fEpsilon * fViewFactor * this.oMT.Const.fStefanBoltzmann * fNodeArea);
             
             %% Define the conductive resistance between each cube element:
-            % The distance between the center of each thermal node
-            fNodeDistance           = fTotalCubeSideLength / this.iNodesPerDirection;
-            fThermalConductivity    = this.oMT.calculateThermalConductivity(this.coNodes{1,1,1});
-            fConductionResistance   = fNodeDistance / (fNodeArea * fThermalConductivity);
+            % The distance between the centers of the thermal nodes
+            fNodeDistance = fTotalCubeSideLength / this.iNodesPerDirection;
+            
+            % Getting the thermal conductivity of the first node, which is
+            % the same for all nodes. 
+            fThermalConductivity = this.oMT.calculateThermalConductivity(this.coNodes{1,1,1});
+            
+            % Now we can calculate the conductive resistance between all
+            % nodes. 
+            fConductionResistance = fNodeDistance / (fNodeArea * fThermalConductivity);
             
             %% Create connections between nodes
             % Now we create the connections between the nodes, boundary
@@ -110,14 +196,23 @@ classdef Example < vsys
                 for iY = 1:this.iNodesPerDirection
                     for iZ = 1:this.iNodesPerDirection
                         
+                        %% External Branches
+                        
+                        % Some logic to determine how many sides of the
+                        % current node are on the outside of the cube.
                         bX_Side_Outfacing = iX == 1 || iX == this.iNodesPerDirection;
                         bY_Side_Outfacing = iY == 1 || iY == this.iNodesPerDirection;
                         bZ_Side_Outfacing = iZ == 1 || iZ == this.iNodesPerDirection;
                         
                         iNumberOfOutfacingSides = bX_Side_Outfacing + bY_Side_Outfacing + bZ_Side_Outfacing;
                         
+                        % Getting the thermal capacity associated with the
+                        % current node.
                         oNode = this.coNodes{iX, iY, iZ}.oCapacity;
                         
+                        % Creating as many radiative thermal branaches as
+                        % there are outward facing sides on the current
+                        % node.
                         for iRadiativeBranch = 1:iNumberOfOutfacingSides
                             sRadiatorConductorName = ['Radiator_',num2str(iRadiativeBranch),'_Node_X', num2str(iX),'_Y', num2str(iY),'_Z', num2str(iZ)];
                             thermal.procs.conductors.radiative(this, sRadiatorConductorName, fRadiativeResistance);
@@ -126,19 +221,16 @@ classdef Example < vsys
                             thermal.branch(this, oNode, {sRadiatorConductorName}, oSpaceCapacity, sRadiatorBranchName);
                         end
                         
+                        %% Internal Branches
                         % For the internal branches, we have to decide what
                         % nodes to connect. Since we loop through all
                         % nodes, we generall only connect from the node
                         % with the lower indices to the one with higher
-                        % indices
+                        % indices.
                         
                         %% X Direction
-                        % We only create the connection to the other
-                        % node, if the node counter is 1, since in the
-                        % othe case the branch is created by the node
-                        % on the other side of the branch
                         if iX ~= this.iNodesPerDirection
-                            aiOtherNode = [(iX +1) , iY, iZ];
+                            aiOtherNode = [(iX + 1) , iY, iZ];
                             oOtherNode  = this.coNodes{aiOtherNode(1), aiOtherNode(2), aiOtherNode(3)}.oCapacity;
 
                             sConductorName = ['Conductor_Node_X',num2str(iX),'_Y',num2str(iY),'_Z',num2str(iZ),'_to_X',num2str(aiOtherNode(1)),'_Y',num2str(aiOtherNode(2)),'_Z',num2str(aiOtherNode(3))];
@@ -175,11 +267,15 @@ classdef Example < vsys
                 end
             end
             
+            %% Heat Sources
+            
+            % Initializing the array of heat sources. 
+            this.aoHeatSources = thermal.heatsource.empty();
             
             % The heatsource is located in the center, so we try finding
             % the middle of all three coordinated to place it
             if mod(this.iNodesPerDirection, 2) == 0
-                % for an even number of nodes, no exact center node exists
+                % For an even number of nodes, no exact center node exists
                 % so we split the heat source between multiple nodes. For
                 % example for a 6x6x6 cube the center is assumed to be a
                 % smaller 2x2x2 cube in the center spanning the indices
@@ -192,12 +288,21 @@ classdef Example < vsys
                 for iX = iLowerCenterIndex:iUpperCenterIndex
                     for iY = iLowerCenterIndex:iUpperCenterIndex
                         for iZ = iLowerCenterIndex:iUpperCenterIndex
-                            
+                            % Getting the current node
                             oNode = this.coNodes{iX, iY, iZ}.oCapacity;
+                            
+                            % Creating a heat source
                             oHeatSource = thermal.heatsource(['Heater_Node_X', num2str(iX),'_Y', num2str(iY),'_Z', num2str(iZ)], 0);
+                            
+                            % Adding the heat source to the node
                             oNode.addHeatSource(oHeatSource);
+                            
+                            % Setting the initial heat flow for the heat
+                            % source
                             oHeatSource.setHeatFlow(this.fHeatFlow / 8);
                             
+                            % Adding the heat source to the property array
+                            this.aoHeatSources(end+1) = oHeatSource;
                         end
                     end
                 end
@@ -206,18 +311,35 @@ classdef Example < vsys
                 % exists (e.g. for a 3x3x3 cube it is node 2,2,2)
                 iCenterNodeIndex = (this.iNodesPerDirection + 1) / 2;
                 
+                % Getting the center node
                 oNode = this.coNodes{iCenterNodeIndex, iCenterNodeIndex, iCenterNodeIndex}.oCapacity;
-                        
+                
+                % Creating a heat source
                 oHeatSource = thermal.heatsource(['Heater_Node_X', num2str(iCenterNodeIndex),'_Y', num2str(iCenterNodeIndex),'_Z', num2str(iCenterNodeIndex)], 0);
+                
+                % Adding the heat source to the node
                 oNode.addHeatSource(oHeatSource);
+                
+                % Setting the initial heat flow for the heat
+                % source
                 oHeatSource.setHeatFlow(this.fHeatFlow);
+                
+                % Setting the property to this heat source. 
+                this.aoHeatSources = oHeatSource;
             end
         end
         
         
         function createSolverStructure(this)
+            % Calling this method in the parent class, that will also call
+            % it on all children of this object.
             createSolverStructure@vsys(this);
+            
+            % Depending on user input we either create an advanced or basic
+            % thermal solver. 
             if this.bAdvancedThermalSolver
+                % The second argument here is the internal time step for
+                % the solver.
                 solver.thermal.multi_branch.advanced.branch(this.aoThermalBranches, 20);
             else
                 solver.thermal.multi_branch.basic.branch(this.aoThermalBranches);
@@ -231,6 +353,16 @@ classdef Example < vsys
             % exec(ute) function for this system
             % Here it only calls its parent's exec function
             exec@vsys(this);
+            
+            % We toggle the heat sources on and off every time exec() is
+            % called. 
+            for iI = 1:length(this.aoHeatSources)
+                if this.aoHeatSources(iI).fHeatFlow == 0
+                    this.aoHeatSources(iI).setHeatFlow(this.fHeatFlow);
+                else
+                    this.aoHeatSources(iI).setHeatFlow(0);
+                end
+            end
             
         end
         
