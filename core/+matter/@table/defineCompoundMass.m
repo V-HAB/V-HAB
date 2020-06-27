6c25430f976f59b192caedd49b27564630af9175
Daniel Pütz <d.puetz@tum.de>
2020-06-27 19:26:26 +0200

    CORE: a bug in the define compound mass function occured if the same compound mass was already present in the matter table. This lead to the iSubstance value beeing increased even though no new substance was added. Now the defineCompoundMass function checks this and correctly increments the iSubstance value

--- core/+matter/@table/defineCompoundMass.m
+++ core/+matter/@table/defineCompoundMass.m
@@ -41,66 +41,74 @@ if rTotalRatio ~= 1
         trBaseComposition.(csComposition{iField}) = trBaseComposition.(csComposition{iField}) / rTotalRatio;
     end
 end
-
-% First we increment the total number of substances
-this.iSubstances = this.iSubstances+1;
-
-% Write new substancename into the cellarray
-this.csSubstances{this.iSubstances} = sCompoundName;
-
-% Add index of new substance to name to index struct
-this.tiN2I.(sCompoundName) = this.iSubstances;
-
-% Get list of substance indices.
-this.csI2N = fieldnames(this.tiN2I);
-
-this.tsS2N.sCompoundName = sCompoundName;
-this.tsN2S.sCompoundName = sCompoundName;
-
-this.abAbsorber(this.iSubstances) = false;
-
-this.abCompound(this.iSubstances) = true;
-
-if nargin > 4
-    this.abEdibleSubstances(this.iSubstances) = bEdibleSubstance;
-else
-    this.abEdibleSubstances(this.iSubstances) = false;
-end
-
 this.ttxMatter.(sCompoundName).trBaseComposition    = trBaseComposition;
-this.ttxMatter.(sCompoundName).bIndividualFile      = false;
-this.ttxMatter.(sCompoundName).sName                = sCompoundName;
-
-arBaseComposition = zeros(1,this.iSubstances);
-for iField = 1:length(csComposition)
-    arBaseComposition(this.tiN2I.(csComposition{iField})) = trBaseComposition.(csComposition{iField});
-end
-
-% this.ttxMatter.(sCompoundName).arBaseComposition   = arBaseComposition;
 this.ttxMatter.(sCompoundName).csComposition        = csComposition;
 
-this.afMolarMass(this.iSubstances) = 0;
-this.afMolarMass(this.iSubstances) = sum(this.afMolarMass .* arBaseComposition);
+% Only do this if the compound mass with this name does not yet exist
+if ~isfield(this.tiN2I, sCompoundName)
+    % First we increment the total number of substances
+    this.iSubstances = this.iSubstances+1;
 
-% For the nutritional energy we assume that is 0 even for foods that
-% consist of multiple parts. These must be split up before calculating
-% their energy content, to ensure it is correct!
-this.afNutritionalEnergy(this.iSubstances) = 0;
-% Charge is also set to 0, if it contains charged substances, this is
-% detected when resolving the compound
-this.aiCharge(this.iSubstances) = 0;
-this.afDissociationConstant(this.iSubstances) = 0;
+    % Write new substancename into the cellarray
+    this.csSubstances{this.iSubstances} = sCompoundName;
 
-% Absorbers store a vector with the same length as the number of substances
-% in the matter table for the adsorption enthalpy. To ensure consistency we
-% also have to add the new substance to these vectors
-for iAbsorber = 1:sum(this.abAbsorber)
-    miAbsorberIndex = find(this.abAbsorber);
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.mfAbsorptionEnthalpy(this.iSubstances) = 0;
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_A0(this.iSubstances) = 0;
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_B0(this.iSubstances) = 0;
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_E(this.iSubstances)  = 0;
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_T0(this.iSubstances) = 0;
-    this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_C0(this.iSubstances) = 0;
+    % Add index of new substance to name to index struct
+    this.tiN2I.(sCompoundName) = this.iSubstances;
+
+    % Get list of substance indices.
+    this.csI2N = fieldnames(this.tiN2I);
+
+    this.tsS2N.sCompoundName = sCompoundName;
+    this.tsN2S.sCompoundName = sCompoundName;
+
+    this.abAbsorber(this.iSubstances) = false;
+
+    this.abCompound(this.iSubstances) = true;
+
+    if nargin > 4
+        this.abEdibleSubstances(this.iSubstances) = bEdibleSubstance;
+    else
+        this.abEdibleSubstances(this.iSubstances) = false;
+    end
+
+    this.ttxMatter.(sCompoundName).bIndividualFile      = false;
+    this.ttxMatter.(sCompoundName).sName                = sCompoundName;
+    
+    arBaseComposition = zeros(1,this.iSubstances);
+    for iField = 1:length(csComposition)
+        arBaseComposition(this.tiN2I.(csComposition{iField})) = trBaseComposition.(csComposition{iField});
+    end
+    this.afMolarMass(this.iSubstances) = 0;
+    this.afMolarMass(this.iSubstances) = sum(this.afMolarMass .* arBaseComposition);
+
+    % For the nutritional energy we assume that is 0 even for foods that
+    % consist of multiple parts. These must be split up before calculating
+    % their energy content, to ensure it is correct!
+    this.afNutritionalEnergy(this.iSubstances) = 0;
+    % Charge is also set to 0, if it contains charged substances, this is
+    % detected when resolving the compound
+    this.aiCharge(this.iSubstances) = 0;
+    this.afDissociationConstant(this.iSubstances) = 0;
+    
+    % Absorbers store a vector with the same length as the number of substances
+    % in the matter table for the adsorption enthalpy. To ensure consistency we
+    % also have to add the new substance to these vectors
+    for iAbsorber = 1:sum(this.abAbsorber)
+        miAbsorberIndex = find(this.abAbsorber);
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.mfAbsorptionEnthalpy(this.iSubstances) = 0;
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_A0(this.iSubstances) = 0;
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_B0(this.iSubstances) = 0;
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_E(this.iSubstances)  = 0;
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_T0(this.iSubstances) = 0;
+        this.ttxMatter.(this.csSubstances{miAbsorberIndex(iAbsorber)}).tAbsorberParameters.tToth.mf_C0(this.iSubstances) = 0;
+    end
+else
+    
+    arBaseComposition = zeros(1,this.iSubstances);
+    for iField = 1:length(csComposition)
+        arBaseComposition(this.tiN2I.(csComposition{iField})) = trBaseComposition.(csComposition{iField});
+    end
+    this.afMolarMass(this.tiN2I.(sCompoundName)) = sum(this.afMolarMass .* arBaseComposition);
+
 end
 end
