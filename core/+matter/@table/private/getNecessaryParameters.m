1ba45f2c0b1b3150516f32871a39026095f74fed
Daniel Kaschubek <d.kaschubek@tum.de>
2020-10-01 11:04:55 +0200

    CORE: found a potential bug in the matter table parameter get function in combination with flow phases. Since the afMass property of a flow phase does not represent its current arMass the previous calculation was wrong, using the arPartialMass property provides the correct results

--- core/+matter/@table/private/getNecessaryParameters.m
+++ core/+matter/@table/private/getNecessaryParameters.m
@@ -16,8 +16,7 @@ if length(varargin) == 1
         if varargin{1}.fMass == 0
             arPartialMass = zeros(1, this.iSubstances);
         else
-            afMass = this.resolveCompoundMass(varargin{1}.afMass, varargin{1}.arCompoundMass);
-            arPartialMass = afMass / varargin{1}.fMass;
+            arPartialMass = this.resolveCompoundMass(varargin{1}.arPartialMass, varargin{1}.arCompoundMass);
         end
         
     elseif strcmp(varargin{1}.sObjectType, 'flow')
