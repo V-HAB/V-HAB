0ce15acb1b679c981f522de84b19f97cbbd5417e
Daniel Pütz <d.puetz@tum.de>
2021-09-14 15:30:10 +0200

    TUTORIALS: Updated Readme for tutorials to provide more information on tutorials and where to start - Updated the thermal and volume_manip tutorials

--- user/+tutorials/+volume_manipulator/+systems/Example.m
+++ user/+tutorials/+volume_manipulator/+systems/Example.m
@@ -1,101 +1,46 @@
 classdef Example < vsys
-    %EXAMPLE Example simulation for a simple flow in V-HAB 2.0
-    %   Two tanks filled with gas at different temperatures and pressures
-    %   with a pipe in between
+    %EXAMPLE Example simulation for a system which contain variable
+    % volumes. And example for this would be the bladder in a space suit,
+    % which is flexible and therefore changes its volume.
     
     properties
-        % This system does not have any properties
     end
     
     methods
         function this = Example(oParent, sName)
-            % Calling the parent constructor. This has to be done for any
-            % class that has a parent. The third parameter defines how
-            % often the .exec() method of this subsystem is called. 
-            % Values can be: 0-inf for interval in [s] (zero means with
-            % lowest time step set for the timer). -1 means with every TICK
-            % of the timer, which is determined by the smallest time step
-            % of any of the systems. Providing a logical 'false' means the
-            % .exec() method is called when the oParent.exec() is executed
-            % (see this .exec() method - always call exec@vsys as well!).
             this@vsys(oParent, sName, 30);
             
         end
         
         
         function createMatterStructure(this)
-            % This function creates all simulation objects in the matter
-            % domain. 
-            
-            % First we always need to call the createMatterStructure()
-            % method of the parent class.
             createMatterStructure@vsys(this);
             
-            % Creating a store, volume 1 m^3
             matter.store(this, 'Tank_1', 1);
+            oTank1Phase = this.toStores.Tank_1.createPhase('air', 1, 293.15);
             
-            % Adding a phase to the store 'Tank_1', 1 m^3 air at 20 deg C
-            oGasPhase = this.toStores.Tank_1.createPhase('air', 1, 293.15);
-            
-            % add the volume manipulator to this phase
-            tutorials.volume_manipulator.components.volumeChanger('Compressor', oGasPhase);
-            
-            % Creating a second store, volume 1 m^3
             matter.store(this, 'Tank_2', 1);
+            oTank2Phase = this.toStores.Tank_2.createPhase('air', 1, 323.15, 0.5, 1e5);
             
-            % Adding a phase to the store 'Tank_2', 2 m^3 air at 50 deg C
-            % Note that we are creating a phase that is twice as voluminous
-            % as the store it is in. This means that the pressure of this
-            % phase will be higher than that of 'Tank_1'
-            oAirPhase = this.toStores.Tank_2.createPhase('air', 1, 323.15, 0.5, 1e5);
+            % Here we add the volume manipulator to the phase inside tank
+            % 1. It basically works the same way a substance manipulator
+            % could be used, you use the path of the manipulator you want
+            % to add and provide the required inputs. The inputs are always
+            % a name and a phase, but can depend on the specific manip,
+            % therefore go to the file for this manip to learn more about
+            % it.
+            tutorials.volume_manipulator.components.volumeChanger('Compressor', oTank1Phase);
             
-            % Adding a pipe to connect the tanks, 1.5 m long, 5 mm in
-            % diameter. The pipe is in the components library and is
-            % derived from the flow-to-flow (f2f) processor class
             components.matter.pipe(this, 'Pipe', 1.5, 0.005);
-            
-            % Creating the flowpath, called a branch in V-HAB, between the
-            % components. 
-            % The input parameter format is always:
-            % system object, phase object, {name(s) of f2f-processor(s)}, phase object
-            matter.branch(this, oGasPhase, {'Pipe'}, oAirPhase, 'Branch');
+            matter.branch(this, oTank1Phase, {'Pipe'}, oTank2Phase, 'Branch');
             
         end
-        
-        
-        function createThermalStructure(this)
-            % This function creates all simulation objects in the thermal
-            % domain. 
-            
-            % First we always need to call the createThermalStructure()
-            % method of the parent class.
-            createThermalStructure@vsys(this);
-            
-            % We need to do nothing else here for this simple model. All
-            % thermal domain objects related to advective (mass-based) heat
-            % transfer will automatically be created by the
-            % setThermalSolvers() method. 
-            % Here one would create simulation objects for radiative and
-            % conductive heat transfer.
-            
-        end
-        
         
         function createSolverStructure(this)
-            % This function creates all of the solver objects required for
-            % a simulation. 
-            
-            % First we always need to call the createSolverStructure()
-            % method of the parent class.
             createSolverStructure@vsys(this);
             
-            % Creating an interval solver object that will solve for the
-            % flow rate of the one branch in this system.
             solver.matter.interval.branch(this.toBranches.Branch);
-
-            % Since we want V-HAB to calculate the temperature changes in
-            % this system we call the setThermalSolvers() method of the
-            % thermal.container class. 
+            
             this.setThermalSolvers();
             
         end
@@ -104,16 +49,8 @@ classdef Example < vsys
      methods (Access = protected)
         
         function exec(this, ~)
-            % exec(ute) function for this system 
-            % This function can be used to change the system state, e.g.
-            % close valves or switch on/off components.
-            
-            % Here it only calls its parent's exec() method
             exec@vsys(this);
             
         end
-        
      end
-    
 end
-
